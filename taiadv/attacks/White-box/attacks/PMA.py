6c2d517af1da8488b8afc2f3a655ab44e8f2caa2
my ssh key <106293780+xieyong0@users.noreply.github.com>
2024-06-03 17:45:38 +0800

    Update PMA.py

--- taiadv/attacks/White-box/attacks/PMA.py
+++ taiadv/attacks/White-box/attacks/PMA.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 from torch.autograd import Variable
@@ -6,24 +7,31 @@ import math
 
 torch.manual_seed(0)
 
-class PMA():
-    def __init__(self, model, epsilon=8./255., num_steps=50,num_random_starts=1,
-                 v_min=0., v_max=1., seed=0, norm='Linf', num_classes=100):
+class MDAttack():
+    def __init__(self, model, epsilon=8./255., num_steps=50,num_random_starts=1, v_min=0., 
+                 v_max=1.,seed=0, norm='Linf', num_classes=100,loss_fn='margin'):
         self.model = model
         self.epsilon = epsilon
         self.num_steps = num_steps
         self.num_random_starts = num_random_starts
         self.v_min = v_min
         self.v_max = v_max
+        self.first_step_size = first_step_size
         self.seed = seed
+        self.norm = norm
         self.num_classes = num_classes
+        self.loss_fn = loss_fn
         self.initial_step_size = 2.0 * epsilon
 
     def perturb(self, x_in, y_in):
-        device = x_in.device
+        #K'
         change_point = self.num_steps/2
+        # loss function
+        assert self.loss_fn in ['mg', 'pm']
         
+        device = x_in.device
         X_adv = x_in.detach().clone()
+
         with torch.no_grad():
             logits = self.model(x_in)
         pred = logits.max(dim=1)[1]
@@ -41,9 +49,13 @@ class PMA():
                     if x_pgd.shape[0]==0 or len(x_pgd.shape)!=4:
                         return X_adv,accs
                     with torch.enable_grad():
-                        logits = self.model(x_pgd)
-                        logits = F.softmax(logits,dim=-1)
-                        
+                        # choose MD
+                        if self.loss_fn == "mg":
+                            logits = self.model(x_pgd)
+                        # choose PMA
+                        if self.loss_fn == "pm":
+                            logits = F.softmax(self.model(x_pgd),dim=-1)
+                            
                         z_y = logits.gather(1, y.view(-1, 1))
                         z_max = logits.gather(1, (logits - torch.eye(self.num_classes)[y.cpu()].to(device) * 9999).argmax(1, keepdim=True))
 
@@ -59,15 +71,16 @@ class PMA():
                         loss.backward()
                         
                         acc = logits.max(1)[1] == y
+                        
                         accs[cor_indexs] = acc
                         X_adv[cor_indexs] = x_pgd.detach()
 
                     if self.use_odi and i < 2:
                         alpha = self.epsilon
                     elif i > change_point:
-                        alpha = self.initial_step_size * 0.5 * (1 + .cos((i-change_point - 1) / (self.num_steps-change_point) * .pi))
+                        alpha = self.initial_step_size * 0.5 * (1 + np.cos((i-change_point - 1) / (self.num_steps-change_point) * np.pi))
                     else:
-                        alpha = self.initial_step_size * 0.5 * (1 + .cos((i - 1) / (self.num_steps-change_point) * .pi))
+                        alpha = self.initial_step_size * 0.5 * (1 + np.cos((i - 1) / (self.num_steps-change_point) * np.pi))
                     eta = alpha * x_pgd.grad.data.sign()
                     x_pgd = x_pgd.detach() + eta.detach()
                     x_pgd = torch.min(torch.max(x_pgd, x_in[cor_indexs] - self.epsilon), x_in[cor_indexs] + self.epsilon)
@@ -76,7 +89,6 @@ class PMA():
                     
                     cor_indexs = accs.nonzero().squeeze()
                     y = y_in[cor_indexs]
-                    
                     if cor_indexs.numel()==0:
                         return X_adv,accs
                     
@@ -87,4 +99,3 @@ class PMA():
                 X_adv[cor_indexs] = x_pgd
                     
         return X_adv, accs
-
