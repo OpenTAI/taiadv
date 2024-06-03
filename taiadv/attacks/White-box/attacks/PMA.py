760067600cd568a16ec4a1be683c0c366bd06bef
my ssh key <106293780+xieyong0@users.noreply.github.com>
2024-06-03 17:05:44 +0800

    Update PMA.py

--- taiadv/attacks/White-box/attacks/PMA.py
+++ taiadv/attacks/White-box/attacks/PMA.py
@@ -1,49 +1,36 @@
-import time
-import sys
 import torch
-import torch.optim as optim
-import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 from torch.autograd import Variable
-from .utils import adv_check_and_update, one_hot_tensor
 import math
 
 torch.manual_seed(0)
 
 class PMA():
-    def __init__(self, model, epsilon=8./255., num_steps=50, step_size=2./255.,
-                 num_random_starts=1, v_min=0., v_max=1., change_point=50,
-                 first_step_size=16./255., seed=0, norm='Linf', num_classes=100,
-                 use_odi=False, use_dlr=False):
+    def __init__(self, model, epsilon=8./255., num_steps=50,num_random_starts=1,
+                 v_min=0., v_max=1., seed=0, norm='Linf', num_classes=100):
         self.model = model
         self.epsilon = epsilon
         self.num_steps = num_steps
-        self.step_size = step_size
         self.num_random_starts = num_random_starts
         self.v_min = v_min
         self.v_max = v_max
-        self.change_point = num_steps/2
-        self.first_step_size = first_step_size
         self.seed = seed
-        self.norm = norm
         self.num_classes = num_classes
-        self.use_odi = use_odi
-        self.use_dlr = use_dlr
-        self.loss_fn = loss_fn
         self.initial_step_size = 2.0 * epsilon
 
     def perturb(self, x_in, y_in):
-        change_point = self.change_point
+        device = x_in.device
+        change_point = self.num_steps/2
+        
         X_adv = x_in.detach().clone()
-
         with torch.no_grad():
             logits = self.model(x_in)
         pred = logits.max(dim=1)[1]
         accs = pred==y_in
         for _ in range(max(self.num_random_starts, 1)):
             for r in range(2):
-                r_noise = torch.FloatTensor(*x_in[accs].shape).uniform_(-self.epsilon, self.epsilon).cuda()
+                r_noise = torch.FloatTensor(*x_in[accs].shape).uniform_(-self.epsilon, self.epsilon).to(device)
                 X_adv[accs] = x_in[accs].data + r_noise
                 
                 cor_indexs = accs.nonzero().squeeze()
@@ -58,10 +45,7 @@ class PMA():
                         logits = F.softmax(logits,dim=-1)
                         
                         z_y = logits.gather(1, y.view(-1, 1))
-                        
-                        #pdb.set_trace()
-                        
-                        z_max = logits.gather(1, (logits - torch.eye(self.num_classes)[y.cpu()].to("cuda") * 9999).argmax(1, keepdim=True))
+                        z_max = logits.gather(1, (logits - torch.eye(self.num_classes)[y.cpu()].to(device) * 9999).argmax(1, keepdim=True))
 
                         if i < 1:
                             loss_per_sample = z_y
@@ -75,16 +59,15 @@ class PMA():
                         loss.backward()
                         
                         acc = logits.max(1)[1] == y
-                        
                         accs[cor_indexs] = acc
                         X_adv[cor_indexs] = x_pgd.detach()
 
                     if self.use_odi and i < 2:
                         alpha = self.epsilon
                     elif i > change_point:
-                        alpha = self.initial_step_size * 0.5 * (1 + np.cos((i-change_point - 1) / (self.num_steps-change_point) * np.pi))
+                        alpha = self.initial_step_size * 0.5 * (1 + .cos((i-change_point - 1) / (self.num_steps-change_point) * .pi))
                     else:
-                        alpha = self.initial_step_size * 0.5 * (1 + np.cos((i - 1) / (self.num_steps-change_point) * np.pi))
+                        alpha = self.initial_step_size * 0.5 * (1 + .cos((i - 1) / (self.num_steps-change_point) * .pi))
                     eta = alpha * x_pgd.grad.data.sign()
                     x_pgd = x_pgd.detach() + eta.detach()
                     x_pgd = torch.min(torch.max(x_pgd, x_in[cor_indexs] - self.epsilon), x_in[cor_indexs] + self.epsilon)
@@ -93,6 +76,7 @@ class PMA():
                     
                     cor_indexs = accs.nonzero().squeeze()
                     y = y_in[cor_indexs]
+                    
                     if cor_indexs.numel()==0:
                         return X_adv,accs
                     
